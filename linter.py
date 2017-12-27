7b090d652cb5900f8fa53a8ffe11baefe6fa5ba0
Jack Cherng <jfcherng@gmail.com>
2017-12-27 09:47:09 +0800

    Allow "extra_flags" to be a list

--- linter.py
+++ linter.py
@@ -34,7 +34,7 @@ def get_project_folder():
     if proj_file:
         return os.path.dirname(proj_file)
 
-    # Use current file's folder when no project file is opened.
+    # use current file's folder when no project file is opened
     proj_file = sublime.active_window().active_view().file_name()
     if proj_file:
         return os.path.dirname(proj_file)
@@ -67,19 +67,27 @@ class Gcc(Linter):
         'c++11',
     }
 
-    default_settings = {
-        'executable': 'gcc',
-        'extra_flags': '',
-        'include_dirs': [],
-    }
-
     common_flags = [
         '-c',
         '-Wall',
         '-O0',
     ]
 
+    default_settings = {
+        'executable': 'gcc',
+        'extra_flags': [],
+        'include_dirs': [],
+    }
+
+    if sublime.platform() == 'windows':
+        garbage_file = tempfile.gettempdir() + r'\SublimeLinter-contrib-gcc.o'
+    else:
+        garbage_file = '/dev/null'
+
+    cmd_template = '{executable} {common_flags} {extra_flags} {include_dirs} -x {c_or_cpp} -o {garbage_file} -'
+
     # SublimeLinter capture settings
+    executable = None
     multiline = True
     syntax = list(c_syntaxes | cpp_syntaxes)
     regex = (
@@ -89,20 +97,14 @@ class Gcc(Linter):
     )
 
     if get_SL_version() == '3':
-        # We would like to bind "executable" later in cmd(self), but
-        # if "executable" is not found here, this linter won't be activated.
+        # Note: This is a dirty hack to use a dynamical "executable".
+        #
+        # If "executable" is not found here, this linter just won't be activated.
         # The following if-branch makes sure an "executable" could be found.
-        if sublime.platform() == 'windows':
-            # Windows OS would have "cmd" (or "explorer") binary in its PATH
-            executable = 'cmd'
-        else:
-            # A non-Windows OS would have "cat" binary in its PATH?
-            executable = 'cat'
-    else:
-        # For SL4, to use a dynamic "executable", just do not set the "executable".
-        executable = None
-
-    cmd_template = '{executable} {common_flags} {extra_flags} {include_dirs} -x {c_or_cpp} -o {garbage_file} -'
+        # The actual "executable" is in the returned command from cmd(self).
+        #
+        # @see https://git.io/vb5Nb
+        executable = 'cmd' if sublime.platform() == 'windows' else 'cat'
 
     def cmd(self):
         """
@@ -112,30 +114,19 @@ class Gcc(Linter):
         and include paths based on settings.
         """
 
-        if self.get_syntax() in self.c_syntaxes:
-            c_or_cpp = 'c'
-        else:
-            c_or_cpp = 'c++'
-
-        if sublime.platform() == 'windows':
-            garbage_file = tempfile.gettempdir() + r'\SublimeLinter-contrib-gcc.o'
-        else:
-            garbage_file = '/dev/null'
-
+        c_or_cpp = 'c' if self.get_syntax() in self.c_syntaxes else 'c++'
         settings = self.get_syntax_specific_settings(c_or_cpp)
 
         return self.cmd_template.format(
             executable = settings['executable'],
             common_flags = ' '.join(self.common_flags),
-            extra_flags = apply_template(settings['extra_flags']),
-            include_dirs = apply_template(
-                ' '.join({
-                    '-I' + shlex.quote(include_dir)
-                    for include_dir in settings['include_dirs']
-                })
-            ),
+            extra_flags = apply_template(' '.join(settings['extra_flags'])),
+            include_dirs = apply_template(' '.join({
+                '-I' + shlex.quote(include_dir)
+                for include_dir in settings['include_dirs']
+            })),
             c_or_cpp = c_or_cpp,
-            garbage_file = shlex.quote(garbage_file),
+            garbage_file = shlex.quote(self.garbage_file),
         )
 
     @classmethod
@@ -164,17 +155,19 @@ class Gcc(Linter):
     def get_syntax_specific_settings(self, c_or_cpp):
         settings = self.get_view_settings()
 
-        base_settings = {
-            'executable'   : settings.get('executable',   self.default_settings['executable']),
-            'extra_flags'  : settings.get('extra_flags',  self.default_settings['extra_flags']),
-            'include_dirs' : settings.get('include_dirs', self.default_settings['include_dirs']),
+        ret = {
+            attr: settings.get(
+                "{}_{}".format(c_or_cpp, attr),
+                settings.get(attr, self.default_settings[attr])
+            )
+            for attr in self.default_settings
         }
 
-        return {
-            'executable'   : settings.get(c_or_cpp + '_executable',   base_settings['executable']),
-            'extra_flags'  : settings.get(c_or_cpp + '_extra_flags',  base_settings['extra_flags']),
-            'include_dirs' : settings.get(c_or_cpp + '_include_dirs', base_settings['include_dirs']),
-        }
+        # always convert "extra_flags" into a list
+        if isinstance(ret['extra_flags'], str):
+            ret['extra_flags'] = shlex.split(ret['extra_flags'])
+
+        return ret
 
 
 class SublimeLinterContribGccRunTests(sublime_plugin.WindowCommand):
